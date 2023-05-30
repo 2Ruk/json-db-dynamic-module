fac83118c46c0d65dafb464762dd373bb5534287
2Ruk <94098919+2Ruk@users.noreply.github.com>
2023-05-30 20:55:15 +0900

    feat: add forRoot module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { JsonDBModule } from './common/json-DB.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    JsonDBModule.forRoot({ path: '/data' }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
