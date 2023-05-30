b20c05d6e8d5c195c45bfc3522a62784f96fc735
2Ruk <94098919+2Ruk@users.noreply.github.com>
2023-05-30 20:52:01 +0900

    feat: type 추상화 ( entity에 대함 )

---
+++ src/common/config/db.config.ts
@@ -0,0 +1,3 @@
+import { DB_MODULES } from '../type/entity.type';
+
+export type Entity = (typeof DB_MODULES)[number];
---
+++ src/common/type/entity.type.ts
@@ -0,0 +1,4 @@
+import { BoardEntity } from '../../entites/board.entity';
+import { UserEntity } from '../../entites/user.entity';
+
+export const DB_MODULES = [BoardEntity, UserEntity];
